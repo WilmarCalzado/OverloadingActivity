1eb36bfa29b3495732113913b5fd602470cde27b
WilmarCalzado <100540672+WilmarCalzado@users.noreply.github.com>
2022-04-14 23:49:42 +0800

    final commit

--- src/com/company/App.java
+++ src/com/company/App.java
@@ -18,30 +18,46 @@ public class App {
     }
 
     /**
-     * Modify the methods in such a way that the Class App
-     * can now have 2 overloaded computeSum() methods.
-     * One computing the sum of two numbers, and one computing the sum
-     * of three numbers.
+     * This method is the overloading that compute the sum of two numbers
+     * and 3 numbers
+     *
      */
-
+    private static int computeSum(int num1, int num2){
+        return num1 + num2;
+    }
+    private static int computeSum(int num1, int num2, int num3) {
+        return num1 + num2 + num3;
+    }
 
     /**
      *
      * Create method that returns the greater integer value
      */
-
+    private static int greaterValue(int x, int y){
+        if(x > y){
+            return x;
+        } else return y;
+    }
 
     /**
      *
      * Create method that returns the greater double value
      */
+        private static double greaterValue(double a, double b){
+            if(a > b){
+                return a;
+            }
+            else return b;
+
+        }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         /**
          * Create Method calls that takes in 2 parameters, and 3 parameters
          * respectively and computes their sum.
          */
-
+        System.out.println("Computing 2 numbers 20 and 30 = " + App.computeSum(20, 30));
+        System.out.println("Computing 3 numbers 20, 30, 50 = " + App.computeSum(20, 30, 50));
         /**
          * Call the greaterValue method with integer parameters
          */
